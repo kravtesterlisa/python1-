e6e76e1824a07fc4c532d8067eb76255c0fabdf8
Lisa <test@test.com>
2016-08-05 21:08:11 +0300

    jetbrains & python

---
+++ class.py
@@ -0,0 +1,20 @@
+#def fn():
+ #   m = 'A'
+  #  while True:
+   #     m *=2
+    #    if len(m)>10:
+     #       break
+      #  yield m
+#gen = fn()
+#print(gen)
+
+#counter = 0
+#for letter in gen:
+ #   print (letter)
+  #  counter +=1
+   # if counter > 10:
+    #    break
+
+dict([ [1, 2], [2, 3], (4, 5) ])
+
+
