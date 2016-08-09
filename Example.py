2669b76f398574edd54d15e5ce72ccddb86f56bf
Lisa <test@test.com>
2016-08-09 23:46:33 +0300

    homework1

---
+++ Example.py
@@ -0,0 +1,9 @@
+a = "zzz"
+
+def func(str):
+    return str.upper()
+
+func("aaa")
+
+
+
---
+++ Homework_P3.py
@@ -0,0 +1,15 @@
+### to_weird
+
+a = 'tyuuiopsd pjgyrfdsa'
+def to_weird(a):
+    i = 0
+    result = ''
+    for x in a:
+        if i % 2 == 0:
+            result += x.upper()
+        else:
+            result += x.lower()
+        i += 1
+    return result
+print to_weird(a)
+
